efb38abddc172df61c70d59e668206058b3e5eac
suisho <suisho000000@gmail.com>
2014-04-29 23:30:29 +0900

    split dimension functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var sizeOf = require('image-size')
 var fs = require("fs")
 var PDFDocument = require('pdfkit')
+var dimension = require("./lib/dimension.js")
 
 var ImageSet = function(images){
   var maxWidth = 0
@@ -36,8 +37,8 @@ var createDoc = function(imgs, options){
       doc.addPage()
     }
     var imgSize = images.sizes[i]
-    var newSize = calcSize(pageSize, imgSize)
-    var offset = calcOffset(pageSize, newSize)
+    var newSize = dimension.calcSize(pageSize, imgSize)
+    var offset = dimension.calcOffset(pageSize, newSize)
     doc.image(img, offset.x, offset.y, newSize)
   })
   return doc
@@ -73,34 +74,3 @@ PDFSlide.prototype.output = function(images, output, cb){
 }
 
 module.exports = PDFSlide
-
-// calcurate size
-var calcSize = function(pageSize, imageSize){
-  if(isLargeSize(pageSize, imageSize)){
-    return imageSize
-  }
-  if(imageSize.width > imageSize.height){
-    return {
-      width : pageSize.width,
-      height : (pageSize.width / imageSize.width) * imageSize.height
-    }
-  }else{
-    return {
-      width : (pageSize.height / imageSize.height) * imageSize.width,
-      height : pageSize.height
-    }
-  }
-}
-
-// return true if a > b
-var isLargeSize = function(a, b){
-  return (a.width > b.width && a.height > b.height)
-}
-
-
-var calcOffset = function(pageSize, imageSize){
-  return {
-    x : (pageSize.width - imageSize.width) / 2,
-    y : (pageSize.height - imageSize.height) / 2
-  }
-}
