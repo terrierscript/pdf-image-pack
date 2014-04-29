e434d86e0c3b4b60754e1219ea9b56915e666b44
suisho <suisho000000@gmail.com>
2014-04-29 21:48:39 +0900

    make image set object

--- index.js
+++ index.js
@@ -1,61 +1,64 @@
 var sizeOf = require('image-size')
 var fs = require("fs")
 var PDFDocument = require('pdfkit')
-var defaults = require("defaults")
-module.exports = function(images, output, opts, cb){
-  var opt = defaults(opts, {
-    fit    : false,
-  })
+
+var ImageSet = function(images){
   var maxWidth = 0
   var maxHeight = 0
-
-  // get sizes
-  var imgSizes = images.map(function(img){
+  this.paths = images
+  this.sizes = images.map(function(img){
     var size = sizeOf(img)
     maxWidth = Math.max(maxWidth, size.width)
     maxHeight = Math.max(maxHeight, size.height)
     return size
   })
 
+  this.maxSize = [maxWidth, maxHeight]
+}
+
+module.exports = function(imgs, stream, options, cb){
+  var images = new ImageSet(imgs)
+
   // auto scaling
-  if(!opt.size){
-    opt.size = [maxWidth, maxHeight]
+  if(!options.size){
+    options.size = images.maxSize
   }
 
-
-  var doc = new PDFDocument(opt)
-  var stream = output
-  if(typeof output == "string"){
-    stream = fs.createWriteStream(output)
-  }
-  doc.pipe(stream)
-  stream.on('error', function(err){
-    cb(err)
-  })
-  stream.on('finish', function(){
-    cb(null, doc)
-  })
+  var doc = new PDFDocument(options)
 
   var pageSize = {
     width:  doc.page.width,
     height: doc.page.height
   }
 
-  images.forEach(function(img, i ){
+  images.paths.forEach(function(img, i ){
     if(i > 0){
       doc.addPage()
     }
-    var imgSize = imgSizes[i]
-    var newSize = calcSize(pageSize, imgSize, opt.fit)
+    var imgSize = images.sizes[i]
+    var newSize = calcSize(pageSize, imgSize)
     var offset = calcOffset(pageSize, newSize)
     doc.image(img, offset.x, offset.y, newSize)
+  })
+
+  if(typeof stream == "string"){
+    stream = fs.createWriteStream(stream)
+  }
+  doc.pipe(stream)
+
+  // bind event
+  stream.on('error', function(err){
+    cb(err)
+  })
+  stream.on('finish', function(){
+    cb(null, doc)
   })
   doc.end()
 }
 
 // calcurate size
-var calcSize = function(pageSize, imageSize, fit){
-  if(!fit && isLargeSize(pageSize, imageSize)){
+var calcSize = function(pageSize, imageSize){
+  if(isLargeSize(pageSize, imageSize)){
     return imageSize
   }
   if(imageSize.width > imageSize.height){
